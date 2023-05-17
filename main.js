bc2bb4e0738072cd9cb8446a4a034490fa7e6b5f
AIRA-2010 <95174567+AIRA-2010@users.noreply.github.com>
2023-05-17 15:18:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ left_wrist_x =0;
 left_wrist_y =0;
 scoreLeftWrist=0;
 scoreRightWrist=0;
-status ="";
+status_1 ="";
+status_2="";
 right_wrist_x =0;
 right_wrist_y =0;
 function preload(){
@@ -10,7 +11,7 @@ function preload(){
     song_2 = loadSound("music2.mp3");
 }
 function setup(){
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(600,500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -29,29 +30,29 @@ function gotPoses(results){
        right_wrist_y = results[0].pose.rightWrist.y;
        console.log("leftWristX = "+left_wrist_x+" leftWristY = "+left_wrist_y+" rightWristX = "+right_wrist_x+" rightWristY = "+right_wrist_y);
        scoreLeftWrist = results[0].pose.keypoints[9].score;
-       status = "set";
        scoreRightWrist = results[0].pose.keypoints[10].score;
-       status = "";
     }
 }
 function draw(){
-    image(video,0,0,300,300);
+    image(video,0,0,600,500);
+    status_1=song_1.isPlaying();
+    status_2=song_2.isPlaying();
     fill("red");
     stroke("red");
     if(scoreLeftWrist > 0.2){
         circle(left_wrist_x, left_wrist_y, 20); 
-        document.getElementById("song_name").innerHTML =="SONG NAME = PETER PAN";
+        song_2.stop();
+        if(status_2 == false){
+            song_1.play(); 
+            document.getElementById("song_name").innerHTML ="SONG NAME = PETER PAN";
+        }
     } 
-    else if(scoreRightWrist > 0.2){
+    if(scoreRightWrist > 0.2){
         circle(right_wrist_x, right_wrist_y, 20);
-        document.getElementById("song_name").innerHTML =="SONG NAME = HARRY POTTER THEME";
+        song_1.stop();
+        if(status_1 == false){
+            song_2.play(); 
+            document.getElementById("song_name").innerHTML ="SONG NAME = HARRY POTTER THEME";
+        }
     } 
-}
-function play(){
-    if(status == "set"){
-        song_1.play();
-    }
-    else if(status == ""){
-        song_2.play();
-    }
 }
